70dac5166ee51d540a14ea0770ada20743503491
Elise Vicknair <35364291+furelisegs@users.noreply.github.com>
2021-11-09 23:26:25 -0600

    Create 8.4:isVerticalPoint.java

---
+++ Ch 8/8.4:isVerticalPoint.java
@@ -0,0 +1,16 @@
+/*Add the following method to your Point class:
+ * 
+ * public boolean isVertical(Point other)
+ * 
+ * Returns true if the given Point lines up vertically with this Point; that is, if their x 
+ * coordinates are the same.
+*/
+
+public class Test {
+	private int x;
+	private int y;
+
+	public boolean isVertical(Point other) {
+		return (other.x == this.x) ;
+
+	}
