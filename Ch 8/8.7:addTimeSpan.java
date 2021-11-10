33ace22e54b317434d93f927fd553149e1331d94
Elise Vicknair <35364291+furelisegs@users.noreply.github.com>
2021-11-10 14:40:23 -0600

    Create 8.7:addTimeSpan.java

---
+++ Ch 8/8.7:addTimeSpan.java
@@ -0,0 +1,25 @@
+/* Add the following method to the TimeSpan class:
+
+
+public void add(TimeSpan span)
+Adds the given amount of time to this time span.
+
+Recall the TimeSpan code from this chapter:
+
+
+public class TimeSpan {
+    private int hours;
+    private int minutes;
+    
+    public void add(int hours, int minutes) { ... }
+    public int getHours() { ... }
+    public int getMinutes() { ... }
+    public String toString() { ... }    
+}
+*/
+  
+  public void add(TimeSpan span) {
+		this.minutes += span.minutes;
+		this.hours += span.hours + this.minutes / 60;
+		this.minutes = this.minutes % 60;
+	}
